88825bb8ee4908b560148129537fffcbe9eb91ac
Igor Ferreira <igorferreiradaniel99@hotmail.com>
2022-01-21 19:26:09 -0300

    Seller entity class

---
+++ src/main/Demo.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Date;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Department obj = new Department(1, "Books");
+        
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+        
+        System.out.println(seller);
+    }
+    
+}
